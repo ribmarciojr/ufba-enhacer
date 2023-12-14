80eb1cf61853944722988f32ca2af2aec0811523
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-12-14 00:20:08 -0300

    feat: func to gen driver and xpaths concat

---
+++ backend/scraper/src/interfaces/driver.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import *
+
+class Driver:
+    def __init__(self):
+        self.__driver = self.__build_driver()
+
+    def get(self, url):
+        self.__driver.get(url)
+
+    def get_active_element(self):
+        active_element = self.__driver.switch_to.active_element
+        return active_element
+
+    def find_element(self, value: str, timeout: float, expected_condition: callable):
+        WebDriverWait(self.__driver, timeout).until(expected_condition((By.XPATH, value)))
+        element = self.__driver.find_element(By.XPATH, value)
+        return element
+
+    def wait_element_disappear(self, value: str, timeout: float, by=By.XPATH):
+        WebDriverWait(self.__driver, timeout).until(EC.invisibility_of_element((by, value)))
+
+    def __build_driver(self):
+        try:
+            options = Options()
+            options.add_argument('--headless')
+            options.add_argument('--window-size=1920,1080')
+            driver = Chrome(options=options)
+            driver.maximize_window()
+        except:
+            return self.__build_driver()
+        return driver
---
+++ backend/scraper/src/utils/xpaths.py
@@ -0,0 +1,11 @@
+class Login:
+    URL = 'https://siac.ufba.br/'
+    CPF = '/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td/font[2]/input'
+    PASS = '/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td/font[2]/input'
+    LOGIN_BUTTON = '/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td/input'
+
+class Disciplinas:
+    URL = 'https://siac.ufba.br/SiacWWW/ConsultarDisciplinasObrigatorias.do'
+    TABLE = '/html/body/table/tbody/tr[3]/td[2]/table[3]'
+    
+    
