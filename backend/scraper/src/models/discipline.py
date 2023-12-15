a14e1cce3e5609f88213dc0fd303e052bdea5c7b
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-12-15 00:10:49 -0300

    feat: added discipline model (namedtuple)

---
+++ backend/scraper/src/models/discipline.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+class Discipline(NamedTuple):
+    code: str
+    name: str
+    workload: int
+    department: str
+    program: str
+    objective: str
+    content: str
+    bibliography: str
